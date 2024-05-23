3c0bbfc358dfec60debdf9fd86dbb96b54532c64
RafaelRigo <rigo.f.rafa@gmail.com>
2024-05-23 12:03:38 -0300

    registrador de produtos pronto

--- registrador de produtos/registrador.py
+++ registrador de produtos/registrador.py
@@ -1,7 +1,9 @@
-import sys
+import sys, os
+import json
 from PySide6.QtCore import Qt, QtMsgType
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QStatusBar, QDialog
 from registro_produtos_ui import Ui_janelaPrincipal
+from tkinter import filedialog
 
 class FormPrincipal(QMainWindow, Ui_janelaPrincipal):
     def __init__(self) -> None:
@@ -11,7 +13,59 @@ class FormPrincipal(QMainWindow, Ui_janelaPrincipal):
         self.setStatusBar(self.statusBar)
         self.show()
         self.situacao = "navegando"
-        self.registrarProduto.clicked.connect(self.registrar_produto)
 
-    def registrar_produto():
-        pass
+        self.registrarProduto.clicked.connect(self.registrar_produto)
+        self.selecionarImagem.clicked.connect(self.selecionar_imagem)
+
+    def selecionar_imagem(self):
+        global imagem
+        tipos_de_arq = ( ('Arquivos jpg', '*.jpg'),
+                         ('Arquivos webp', '*.webp'),
+                         ('Arquivos png', '*.png'),
+                         ('Arquivos gif', '*.gif'),
+                         ('Arquivos jfif', '*.jfif'),
+                         ('Qualquer arquivo', '*.*') )
+        nome_arq = filedialog.askopenfilename(
+            title='Selecione a imagem',
+            initialdir=r'../imagens',
+            filetypes=tipos_de_arq
+        )
+        
+        dir_list = nome_arq.split("/")
+        caminho = f"../../{dir_list[-2]}/{dir_list[-1]}"
+        
+        imagem = caminho
+
+    def registrar_produto(self):
+        with open("codigo/produtos/produtos.json", encoding='utf-8') as f:
+            json_produtos = json.load(f)
+        
+        maior_id = 0
+        for produto in json_produtos:
+            if int(produto["id"]) > maior_id:
+                maior_id = produto["id"]
+        
+        id = maior_id + 1
+        nome = self.nomeProduto.text()
+        categoria = self.categoriaProduto.currentText()
+        preco = self.precoProduto.value()
+        quantidade = self.quantidadeProduto.text()
+
+        obj = {
+            "id": id,
+            "nome": nome,
+            "categoria": categoria,
+            "preco": preco,
+            "quantidade": quantidade,
+            "imagem": imagem
+        }
+
+        json_produtos.append(obj)
+        json_produtos = json.dumps( json_produtos, indent=4)
+
+        with open("codigo/produtos/produtos.json", "w", encoding='utf-8') as f:
+            f.write(json_produtos)
+
+application = QApplication(sys.argv)
+janela = FormPrincipal()
+application.exec()
--- registrador de produtos/registro_produtos_ui.py
+++ registrador de produtos/registro_produtos_ui.py
@@ -15,54 +15,80 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QDoubleSpinBox, QLabel,
-    QLineEdit, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QDoubleSpinBox, QGridLayout,
+    QLabel, QLineEdit, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
 
 class Ui_janelaPrincipal(object):
     def setupUi(self, janelaPrincipal):
         if not janelaPrincipal.objectName():
             janelaPrincipal.setObjectName(u"janelaPrincipal")
-        janelaPrincipal.resize(264, 285)
+        janelaPrincipal.resize(291, 285)
         self.centralwidget = QWidget(janelaPrincipal)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.label = QLabel(self.centralwidget)
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(20, 20, 248, 158))
+        self.gridLayout = QGridLayout(self.widget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.widget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(20, 20, 121, 16))
-        self.nomeProduto = QLineEdit(self.centralwidget)
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.nomeProduto = QLineEdit(self.widget)
         self.nomeProduto.setObjectName(u"nomeProduto")
-        self.nomeProduto.setGeometry(QRect(130, 20, 113, 20))
-        self.categoriaProduto = QComboBox(self.centralwidget)
+
+        self.gridLayout.addWidget(self.nomeProduto, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.categoriaProduto = QComboBox(self.widget)
         self.categoriaProduto.addItem("")
         self.categoriaProduto.addItem("")
         self.categoriaProduto.addItem("")
         self.categoriaProduto.setObjectName(u"categoriaProduto")
-        self.categoriaProduto.setGeometry(QRect(130, 50, 111, 22))
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(20, 50, 121, 16))
-        self.label_3 = QLabel(self.centralwidget)
+
+        self.gridLayout.addWidget(self.categoriaProduto, 1, 1, 1, 1)
+
+        self.label_3 = QLabel(self.widget)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(20, 80, 111, 16))
-        self.precoProduto = QDoubleSpinBox(self.centralwidget)
+
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.precoProduto = QDoubleSpinBox(self.widget)
         self.precoProduto.setObjectName(u"precoProduto")
-        self.precoProduto.setGeometry(QRect(130, 80, 111, 22))
-        self.label_4 = QLabel(self.centralwidget)
+
+        self.gridLayout.addWidget(self.precoProduto, 2, 1, 1, 1)
+
+        self.label_4 = QLabel(self.widget)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(20, 110, 111, 16))
-        self.quantidadeProduto = QLineEdit(self.centralwidget)
+
+        self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+
+        self.quantidadeProduto = QLineEdit(self.widget)
         self.quantidadeProduto.setObjectName(u"quantidadeProduto")
-        self.quantidadeProduto.setGeometry(QRect(130, 110, 113, 20))
-        self.selecionarImagem = QPushButton(self.centralwidget)
+
+        self.gridLayout.addWidget(self.quantidadeProduto, 3, 1, 1, 1)
+
+        self.selecionarImagem = QPushButton(self.widget)
         self.selecionarImagem.setObjectName(u"selecionarImagem")
-        self.selecionarImagem.setGeometry(QRect(130, 140, 111, 23))
-        self.registrarProduto = QPushButton(self.centralwidget)
+
+        self.gridLayout.addWidget(self.selecionarImagem, 4, 1, 1, 1)
+
+        self.registrarProduto = QPushButton(self.widget)
         self.registrarProduto.setObjectName(u"registrarProduto")
-        self.registrarProduto.setGeometry(QRect(70, 180, 101, 41))
+
+        self.gridLayout.addWidget(self.registrarProduto, 5, 0, 1, 2)
+
         janelaPrincipal.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(janelaPrincipal)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 264, 21))
+        self.menubar.setGeometry(QRect(0, 0, 291, 21))
         janelaPrincipal.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(janelaPrincipal)
         self.statusbar.setObjectName(u"statusbar")
@@ -76,11 +102,11 @@ class Ui_janelaPrincipal(object):
     def retranslateUi(self, janelaPrincipal):
         janelaPrincipal.setWindowTitle(QCoreApplication.translate("janelaPrincipal", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("janelaPrincipal", u"Nome do produto:", None))
+        self.label_2.setText(QCoreApplication.translate("janelaPrincipal", u"Categoria do produto:", None))
         self.categoriaProduto.setItemText(0, QCoreApplication.translate("janelaPrincipal", u"fruta", None))
         self.categoriaProduto.setItemText(1, QCoreApplication.translate("janelaPrincipal", u"legume", None))
         self.categoriaProduto.setItemText(2, QCoreApplication.translate("janelaPrincipal", u"verdura", None))
 
-        self.label_2.setText(QCoreApplication.translate("janelaPrincipal", u"Categoria do produto:", None))
         self.label_3.setText(QCoreApplication.translate("janelaPrincipal", u"Pre\u00e7o:", None))
         self.label_4.setText(QCoreApplication.translate("janelaPrincipal", u"Quantidade:", None))
         self.selecionarImagem.setText(QCoreApplication.translate("janelaPrincipal", u"selecionar imagem", None))
