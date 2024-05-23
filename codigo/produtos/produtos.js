8879e34769e75adb494a235367203b08f246186d
RafaelRigo <rigo.f.rafa@gmail.com>
2024-05-22 22:18:40 -0300

    carregar produtos a partir de arquivo json e organização no javascript

---
+++ codigo/produtos/produtos.js
@@ -0,0 +1,71 @@
+let c = 1
+function reset(){
+    c = 1
+}
+
+function somar(classe){
+    c += 1
+    document.querySelector('.'+classe).innerHTML = c
+}
+
+function sub(classe){
+    if (c > 1){
+        c -= 1
+    }
+    document.querySelector('.'+classe).innerHTML = c
+}
+
+function url() {
+    let urlParams = new URLSearchParams(window.location.search)
+    let valorInput = urlParams.get('input')
+    document.getElementById('txt-buscar').value = valorInput;
+}
+
+function filtro() {           
+    let classes = ['fruta', 'legume', 'verdura'];
+
+    for (let alimento of classes) {
+        let al = document.querySelectorAll('.' + alimento + 's');
+        if (document.querySelector('#' + alimento).checked === false) {
+            for (let i of al) {
+                i.style.display = 'none';
+            }
+        } else {
+            for (let j of al) {
+                j.style.display = 'block';
+            }
+        }
+    }
+}
+
+const carregarProdutos = () => {
+    fetch('produtos.json')
+        .then(response => response.json())
+        .then(data => {
+
+            let produtos = data;
+            let div = document.getElementById('mercadoria')
+            let html = '';
+
+            produtos.forEach(produto => {
+                html += `
+                <div class="it ${produto["categoria"]}s">
+                <img class="img" src="${produto["imagem"]}">
+                <div class="info">
+                <h2>${produto["nome"]} - ${produto["quantidade"]}</h2>
+                <h4>${produto["categoria"].charAt().toUpperCase() + produtos["categoria"].slice(1)}</h4>
+                <h3>R$ 10,00</h3>
+                <button class="qtd" onclick="somar('cont${produtos["id"]}')">+</button>
+                <p class="cont${produtos["id"]}">1</p>
+                <button class="qtd" onclick="sub('cont${produtos["id"]}')">-</button>
+                <button class="add" onclick="reset()">adicionar</button>
+                </div></div>
+                `
+            });
+
+            div.innerHTML = html
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
